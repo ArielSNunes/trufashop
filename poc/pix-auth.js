cccefbb87f3db7f1a4cdb3902de59636fc26bf70
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-05-21 09:11:45 -0300

    Chamada da função para cobrar o pix

--- poc/pix-auth.js
+++ poc/pix-auth.js
@@ -1,6 +1,4 @@
 const path = require('path')
-const https = require('https')
-const axios = require('axios')
 const fs = require('fs')
 require('dotenv').config({
 	path: path.resolve(__dirname, '..', '.env.homologacao')
@@ -11,7 +9,8 @@ const {
 	GN_ENV,
 	GN_CLIENT_ID,
 	GN_CLIENT_SECRET,
-	GN_CERTIFICATE_FILE
+	GN_CERTIFICATE_FILE,
+	GN_CHAVE_PIX
 } = process.env
 
 const getToken = async () => {
@@ -28,10 +27,15 @@ const getToken = async () => {
 		grant_type: 'client_credentials'
 	})
 
-	const token = await gnTransaction.getToken(data)
+	await gnTransaction.getToken(data)
 
-	console.log(token)
-
+	const cobranca = await gnTransaction.askForPix({
+		calendario: { expiracao: 3600 },
+		devedor: { cpf: '44189727814', nome: 'Ariel dos Santos Nunes' },
+		valor: { original: '1.50' },
+		chave: GN_CHAVE_PIX,
+		solicitacaoPagador: 'Cobrança dos serviços prestados'
+	})
 }
 
 getToken()
