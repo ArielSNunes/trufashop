c095b47e3ec08f317cf73df08dc88026e8bb31e1
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-05-21 08:11:38 -0300

    Criação de request para capturar o token de autenticação

---
+++ poc/helpers.js
@@ -0,0 +1,10 @@
+const urls = {
+    producao: 'https://api-pix.gerencianet.com.br',
+    homologacao: 'https://api-pix-h.gerencianet.com.br'
+}
+
+const getUrl = (env) => urls[env] || urls.homologacao
+
+module.exports = {
+    getUrl
+}
--- poc/pix-auth.js
+++ poc/pix-auth.js
@@ -1,5 +1,63 @@
+const path = require('path')
+const https = require('https')
+const axios = require('axios')
+const fs = require('fs')
 require('dotenv').config({
-    path: '../.env.homologacao'
+    path: path.resolve(__dirname, '..', '.env.homologacao')
 })
 
-console.log(process.env.GN_CLIENT_ID)
+const helpers = require('./helpers')
+const {
+    GN_ENV,
+    GN_CLIENT_ID,
+    GN_CLIENT_SECRET,
+    GN_CERTIFICATE_FILE
+} = process.env
+
+const getToken = async () => {
+    const certificate = fs.readFileSync(
+        path.resolve(__dirname, '..', GN_CERTIFICATE_FILE)
+    )
+    const credentials = {
+        client_id: GN_CLIENT_ID,
+        client_secret: GN_CLIENT_SECRET
+    }
+
+    const data = JSON.stringify({
+        grant_type: 'client_credentials'
+    })
+
+    const dataCredentials = [
+        credentials.client_id,
+        ':',
+        credentials.client_secret
+    ].join('')
+
+    const auth = Buffer.from(dataCredentials).toString('base64')
+    const agent = new https.Agent({
+        pfx: certificate,
+        passphrase: ''
+    })
+
+    const axiosConfig = {
+        method: 'POST',
+        url: helpers.getUrl(GN_ENV) + '/oauth/token',
+        headers: {
+            Authorization: `Basic ${auth}`,
+            'Content-type': 'application/json'
+        },
+        httpsAgent: agent,
+        data
+    }
+
+    try {
+        const response = await axios(axiosConfig)
+        const json = response.data
+        console.log(json)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+getToken()
